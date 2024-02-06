656e24a97ad67ac0ff62c7a5f3020093fe4c81d9
Am1G3 <48615423+Am1G3@users.noreply.github.com>
2024-02-07 00:16:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showYesPage() {
+    document.getElementById('yesPage').style.display = 'block';
+    document.getElementById('noPage').style.display = 'none';
+}
+
+function showNoPage() {
+    document.getElementById('yesPage').style.display = 'none';
+    document.getElementById('noPage').style.display = 'block';
+}
