7f102587a8b69091d32d7513ec3b735b72de7586
Am1G3 <48615423+Am1G3@users.noreply.github.com>
2024-02-07 00:31:42 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showYesPage() {
-    document.getElementById('yesPage').style.display = 'block';
-    document.getElementById('noPage').style.display = 'none';
-}
-
-function showNoPage() {
-    document.getElementById('yesPage').style.display = 'none';
-    document.getElementById('noPage').style.display = 'block';
-}
